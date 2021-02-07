a6351f69673643e1be67610b9730d8e33df9f7d5
Lindsay <lspurka@gmail.com>
2021-02-07 12:28:25 -0500

    removed test info

--- vcf_tools.py
+++ vcf_tools.py
@@ -4,6 +4,93 @@ import allel
 import os
 
 
+def get_vcf_df(vcf_path, vcf_fields=None, alt_number=1):
+    """ Returns a non-empty dataframe of the parsed VCF 
+    :param str vcf_path: path to VCF file
+    :param list vcf_fields: optional, list of additional VCF fields to output, default is:
+        ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER_PASS']
+    :param int alt_number: Looks for the number of specified alt alleles in the VCF
+    :return pandas.DataFrame: dataframe of VCF variants
+    """
+
+    # Check that VCF file has variants
+    # if vcf_fields is set to None, the default columns from allel will be output
+]
+    # TODO fix below logic to be more understandable
+    default_fields = ["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER_PASS"]
+    if vcf_fields == ["*"]:
+        vcf_fields = "*"
+    elif vcf_fields:
+        vcf_fields = default_fields + vcf_fields
+    else:
+        vcf_fields = default_fields
+
+    vcf_df = allel.vcf_to_dataframe(vcf_path, fields=vcf_fields, alt_number=alt_number)
+    assert not vcf_df.empty, f"ERROR: No VCF contents for: {vcf_path}"
+
+    # Check that columns from parsed VCF have variant rows
+    # vcf_df.dropna(axis=1, how="all", inplace=True)
+    # if vcf_fields:
+    #     assert not vcf_df.empty, "ERROR: No results, Double check the specified vcf_fields."
+    # assert not vcf_df.empty, "ERROR: No results.  Check inputs."
+
+    return vcf_df
+
+
+def get_vcf_variants(vcf_path, vcf_fields=None, alt_number=1, start_position=None, end_position=None, 
+        pad=None, ref_alleles=None, alt_alleles=None, qual_threshold=None, variant_types=None):
+    """ Tool to get variants in a VCF based on any of: start position, end position, ref allele, alt allele, quality 
+    score, variant type. If no filters are specified, the function will return a dataframe of the parsed VCF.  This 
+    tool works best for small scale variants, rather than CNV VCFs.
+    :param str vcf_path: path to VCF file
+    :param list vcf_fields: optional, list of additional VCF fields to output, default is:
+        ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER_PASS'].  If the user wants all columns output, use ["*"]
+    # TODO add in 'alt_number' functionality. `numalt` is a helpful column output by allel.vcf_to_dataframe
+    :param list alt_number: optional, if >1, ALT columns will be named 'ALT_1', 'ALT_2', 'ALT_3', etc, otherwise just 
+        one will be named 'ALT'
+    :param int start_position: optional, start position to filter variants by.
+    :param int end_position: optional, end position to filter variants by.
+    :param int pad: optional, pads the pos and end param if either are specified.
+    :param list(str) ref_alleles: optional, specific reference alleles to filter variants by
+    :param list(str) alt_alleles: optional, specific alternate alleles to filter variants by
+    :param float qual_threshold: optional, quality score threshold, return results at or above the threshold.
+    :param list(str) variant_types: optional, list of variant types to filter variants by, any of: 'substitution', 
+        'indel', 'deletion', 'insertion', 'delins'. Delins variants will be included if any of 'indel', 'deletion', 
+        'insertion', 'delins' are specified.
+    :return pandas.DataFrame: dataframe of VCF variants
+    """
+
+    # Check user input, reformat lists to uppercase
+    vcf_fields, alt_allele_columns, ref_alleles, alt_alleles, variant_types = _check_and_reformat_user_inputs(
+            vcf_path=vcf_path, vcf_fields=vcf_fields, start_position=start_position, end_position=end_position, pad=pad, 
+            ref_alleles=ref_alleles, alt_alleles=alt_alleles, qual_threshold=qual_threshold, 
+            variant_types=variant_types)
+
+    # Get non-empty dataframe from parsed VCF
+    vcf_df = get_vcf_df(vcf_path=vcf_path, vcf_fields=vcf_fields, alt_number=alt_number)
+
+    # Add allele sequence length columns for the ref and alt allele(s)
+    vcf_df["REF_len"] = vcf_df["REF"].str.len()
+    vcf_df["ALT_len"] = vcf_df["ALT"].str.len()
+
+    # Add END position column
+    vcf_df["END"] = vcf_df["POS"] + vcf_df["REF_len"] - 1 # subtract one since VCFs are 1-indexed
+
+    # Add column(s) which are the difference between the alt(s) and ref allele sequence lengths
+    vcf_df["ALT_diff"] = vcf_df["ALT_len"] - vcf_df["REF_len"]
+
+    # Add variant type column, which determines if the variant is specifically one of: 'substitution', 'deletion', 
+    # 'insertion'
+    vcf_df = add_var_type_col(vcf_df)
+
+
+    filtered_df = _filter_vcf_df(vcf_df=vcf_df, start_position=start_position, end_position=end_position, pad=pad, 
+            ref_alleles=ref_alleles, alt_alleles=alt_alleles, qual_threshold=qual_threshold, 
+            variant_types=variant_types)
+
+
+
+
 def _check_and_reformat_user_inputs(vcf_path, vcf_fields, alt_number, start_position, end_position, pad, 
         ref_alleles, alt_alleles, qual_threshold, variant_types):
     """ Check inputs to function get_vcf_variants """
@@ -109,103 +196,8 @@ def add_var_type_col(vcf_df):
     pass
 
 
-def get_vcf_df(vcf_path, vcf_fields=None, alt_number=1):
-    """ Returns a non-empty dataframe of the parsed VCF 
-    :param str vcf_path: path to VCF file
-    :param list vcf_fields: optional, list of additional VCF fields to output, default is:
-        ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER_PASS']
-    :param int alt_number: Looks for the number of specified alt alleles in the VCF
-    :return pandas.DataFrame: dataframe of VCF variants
-    """
-
-    # Check that VCF file has variants
-    # if vcf_fields is set to None, the default columns from allel will be output
-]
-    # TODO fix below logic to be more understandable
-    default_fields = ["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER_PASS"]
-    if vcf_fields == ["*"]:
-        vcf_fields = "*"
-    elif vcf_fields:
-        vcf_fields = default_fields + vcf_fields
-    else:
-        vcf_fields = default_fields
-
-    vcf_df = allel.vcf_to_dataframe(vcf_path, fields=vcf_fields, alt_number=alt_number)
-    assert not vcf_df.empty, f"ERROR: No VCF contents for: {vcf_path}"
-
-    # Check that columns from parsed VCF have variant rows
-    # vcf_df.dropna(axis=1, how="all", inplace=True)
-    # if vcf_fields:
-    #     assert not vcf_df.empty, "ERROR: No results, Double check the specified vcf_fields."
-    # assert not vcf_df.empty, "ERROR: No results.  Check inputs."
-
-    return vcf_df
-
-
-def get_vcf_variants(vcf_path, vcf_fields=None, alt_number=1, start_position=None, end_position=None, 
-        pad=None, ref_alleles=None, alt_alleles=None, qual_threshold=None, variant_types=None):
-    """ Tool to get variants in a VCF based on any of: start position, end position, ref allele, alt allele, quality 
-    score, variant type. If no filters are specified, the function will return a dataframe of the parsed VCF.  This 
-    tool works best for small scale variants, rather than CNV VCFs.
-    :param str vcf_path: path to VCF file
-    :param list vcf_fields: optional, list of additional VCF fields to output, default is:
-        ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER_PASS'].  If the user wants all columns output, use ["*"]
-    # TODO add in 'alt_number' functionality. `numalt` is a helpful column output by allel.vcf_to_dataframe
-    :param list alt_number: optional, if >1, ALT columns will be named 'ALT_1', 'ALT_2', 'ALT_3', etc, otherwise just 
-        one will be named 'ALT'
-    :param int start_position: optional, start position to filter variants by.
-    :param int end_position: optional, end position to filter variants by.
-    :param int pad: optional, pads the pos and end param if either are specified.
-    :param list(str) ref_alleles: optional, specific reference alleles to filter variants by
-    :param list(str) alt_alleles: optional, specific alternate alleles to filter variants by
-    :param float qual_threshold: optional, quality score threshold, return results at or above the threshold.
-    :param list(str) variant_types: optional, list of variant types to filter variants by, any of: 'substitution', 
-        'indel', 'deletion', 'insertion', 'delins'. Delins variants will be included if any of 'indel', 'deletion', 
-        'insertion', 'delins' are specified.
-    :return pandas.DataFrame: dataframe of VCF variants
-    """
-
-    # Check user input, reformat lists to uppercase
-    vcf_fields, alt_allele_columns, ref_alleles, alt_alleles, variant_types = _check_and_reformat_user_inputs(
-            vcf_path=vcf_path, vcf_fields=vcf_fields, start_position=start_position, end_position=end_position, pad=pad, 
-            ref_alleles=ref_alleles, alt_alleles=alt_alleles, qual_threshold=qual_threshold, 
-            variant_types=variant_types)
-
-    # Get non-empty dataframe from parsed VCF
-    vcf_df = get_vcf_df(vcf_path=vcf_path, vcf_fields=vcf_fields, alt_number=alt_number)
-
-    # Add allele sequence length columns for the ref and alt allele(s)
-    vcf_df["REF_len"] = vcf_df["REF"].str.len()
-    vcf_df["ALT_len"] = vcf_df["ALT"].str.len()
-
-    # Add END position column
-    vcf_df["END"] = vcf_df["POS"] + vcf_df["REF_len"] - 1 # subtract one since VCFs are 1-indexed
-
-    # Add column(s) which are the difference between the alt(s) and ref allele sequence lengths
-    vcf_df["ALT_diff"] = vcf_df["ALT_len"] - vcf_df["REF_len"]
-
-    # Add variant type column, which determines if the variant is specifically one of: 'substitution', 'deletion', 
-    # 'insertion'
-    vcf_df = add_var_type_col(vcf_df)
-
-
-    filtered_df = _filter_vcf_df(vcf_df=vcf_df, start_position=start_position, end_position=end_position, pad=pad, 
-            ref_alleles=ref_alleles, alt_alleles=alt_alleles, qual_threshold=qual_threshold, 
-            variant_types=variant_types)
-
-
-
 # TODO tests
 # TODO accommodate alt_number > 1
 
 
-vcf_path = "/Users/lindsay/code/gnomad.exomes.r2.1.1.sites.21_subset.vcf"
-vcf_fields = None
-alt_number = 1
-start_position = None
-end_position = None
-pad = None
-ref_alleles = None
-alt_alleles = None
-qual_threshold = None
-variant_types = None
+
