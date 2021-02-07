ec1acb63bf1af6bdf555d4e9ca0ec3e268a01e6e
Lindsay <lspurka@gmail.com>
2021-02-07 14:09:57 -0500

    added in add_columns_to_vcf_df, substitution->snv

--- vcf_tools.py
+++ vcf_tools.py
@@ -15,7 +15,7 @@ def get_vcf_df(vcf_path, vcf_fields=None, alt_number=1):
 
     # Check that VCF file has variants
     # if vcf_fields is set to None, the default columns from allel will be output
-]
+
     # TODO fix below logic to be more understandable
     default_fields = ["CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER_PASS"]
     if vcf_fields == ["*"]:
@@ -37,6 +37,46 @@ def get_vcf_df(vcf_path, vcf_fields=None, alt_number=1):
     return vcf_df
 
 
+def add_columns_to_vcf_df(vcf_df):
+    """ TODO docstring """
+
+    def _determine_var_type(row):
+        ref_len, alt_len, alt_diff = row["REF_len"], row["ALT_len"], row["ALT_diff"]
+
+        if row["ALT_diff"] == 0 and row["REF_len"] == 1:
+            return "snv"
+
+
+        
+        alt_diff = row["ALT_diff"]
+
+
+
+    def _add_var_type_col(vcf_df):
+        """ TODO docstring """
+        vcf_df["is_snv"] = vcf_df.apply()
+
+        vcf_df["is_snv"] = (vcf_df["ALT_diff"] == 0 and vcf_df["REF_len"] == 1)
+        vcf_df["is_snv"] = vcf_df["ALT_len"] == 1
+        vcf_df["is_snv"] = vcf_df["ALT_len"] == vcf_df["REF_len"] == 1
+
+        
+
+    # Add allele sequence length columns for the ref and alt allele(s)
+    vcf_df["REF_len"] = vcf_df["REF"].str.len()
+    vcf_df["ALT_len"] = vcf_df["ALT"].str.len()
+
+    # Add END position column
+    vcf_df["END"] = vcf_df["POS"] + vcf_df["REF_len"] - 1 # subtract one since VCFs are 1-indexed
+
+    # Add column(s) which are the difference between the alt(s) and ref allele sequence lengths
+    vcf_df["ALT_diff"] = vcf_df["ALT_len"] - vcf_df["REF_len"]
+
+    # Add variant type column, which determines if the variant is specifically one of: 'snv', 'delins', 'deletion', 
+    # 'insertion'
+    vcf_df["VAR_TYPE"] = vcf_df.apply(_determine_var_type, axis=1)
+
+
 def get_vcf_variants(vcf_path, vcf_fields=None, alt_number=1, start_position=None, end_position=None, 
         pad=None, ref_alleles=None, alt_alleles=None, qual_threshold=None, variant_types=None):
     """ Tool to get variants in a VCF based on any of: start position, end position, ref allele, alt allele, quality 
@@ -54,7 +94,7 @@ def get_vcf_variants(vcf_path, vcf_fields=None, alt_number=1, start_position=Non
     :param list(str) ref_alleles: optional, specific reference alleles to filter variants by
     :param list(str) alt_alleles: optional, specific alternate alleles to filter variants by
     :param float qual_threshold: optional, quality score threshold, return results at or above the threshold.
-    :param list(str) variant_types: optional, list of variant types to filter variants by, any of: 'substitution', 
+    :param list(str) variant_types: optional, list of variant types to filter variants by, any of: 'snv', 
         'indel', 'deletion', 'insertion', 'delins'. Delins variants will be included if any of 'indel', 'deletion', 
         'insertion', 'delins' are specified.
     :return pandas.DataFrame: dataframe of VCF variants
@@ -69,19 +109,8 @@ def get_vcf_variants(vcf_path, vcf_fields=None, alt_number=1, start_position=Non
     # Get non-empty dataframe from parsed VCF
     vcf_df = get_vcf_df(vcf_path=vcf_path, vcf_fields=vcf_fields, alt_number=alt_number)
 
-    # Add allele sequence length columns for the ref and alt allele(s)
-    vcf_df["REF_len"] = vcf_df["REF"].str.len()
-    vcf_df["ALT_len"] = vcf_df["ALT"].str.len()
-
-    # Add END position column
-    vcf_df["END"] = vcf_df["POS"] + vcf_df["REF_len"] - 1 # subtract one since VCFs are 1-indexed
-
-    # Add column(s) which are the difference between the alt(s) and ref allele sequence lengths
-    vcf_df["ALT_diff"] = vcf_df["ALT_len"] - vcf_df["REF_len"]
-
-    # Add variant type column, which determines if the variant is specifically one of: 'substitution', 'deletion', 
-    # 'insertion'
-    vcf_df = add_var_type_col(vcf_df)
+    # Add columns to enable filtering
+    vcf_df = add_columns_to_vcf_df(vcf_df)
 
 
     filtered_df = _filter_vcf_df(vcf_df=vcf_df, start_position=start_position, end_position=end_position, pad=pad, 
@@ -152,9 +181,11 @@ def _check_and_reformat_user_inputs(vcf_path, vcf_fields, alt_number, start_posi
     return vcf_fields, ref_alleles, alt_alleles, variant_types
 
 
-def filter_vcf_df(vcf_df, vcf_fields, start_position, end_position, pad, ref_alleles, alt_alleles, qual_threshold, 
+def filter_vcf_df(vcf_df, start_position, end_position, pad, ref_alleles, alt_alleles, qual_threshold, 
         variant_types):
-    """ Filter VCF dataframe """
+    """ Filter VCF dataframe 
+    # TODO finish docstring
+    """
 
     if start_position:
         assert type(start_position)
@@ -171,9 +202,7 @@ def filter_vcf_df(vcf_df, vcf_fields, start_position, end_position, pad, ref_all
         vcf_df = vcf_df.query("REF in @ref_alleles")
 
     if alt_alleles:
-        for col in vcf_df.columns:
-            if "ALT" in col:
-                vcf_df = vcf_df.query(f"{col} in @alt_alleles")
+        vcf_df = vcf_df.query("ALT in @alt_alleles")
 
     if qual_threshold:
         vcf_df = vcf_df.query("QUAL >= @qual_threshold")
@@ -181,14 +210,12 @@ def filter_vcf_df(vcf_df, vcf_fields, start_position, end_position, pad, ref_all
     if variant_types:
         pass
         # df = pd.DataFrame()
-        # if "SUBSTITUTION" in variant_types:
+        # if "snv" in variant_types:
         #     filtered_df = vcf_df.query("POS == END")
         #     df = pd.concat([df, filtered_df])
         # if ("DELETION" or "DELINS") in variant_types:
 
-
-def add_var_type_col(vcf_df):
-    pass
+    
 
 
 # TODO tests
